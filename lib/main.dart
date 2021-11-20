f5690cc2f6da62eb29db3cde05d38ea73b1b77ab
Jitender <kumarjitu199@gmail.com>
2021-11-20 14:14:45 +0530

    flow chart till progress

--- lib/main.dart
+++ lib/main.dart
@@ -6,16 +6,16 @@ void main() {
       theme:
           ThemeData(brightness: Brightness.dark, primaryColor: Colors.blueGrey),
       home: Scaffold( body: Container(
-        decoration: BoxDecoration(
+        decoration: const BoxDecoration(
           image: DecorationImage(
             image: NetworkImage('https://i.pinimg.com/originals/13/b1/78/13b1788b58e34a0eea149aab7219fb7f.jpg'),
             fit: BoxFit.cover,
           ),
-        ),
+       ),
       ),
 
         appBar: AppBar(
-          flexibleSpace: const Image(
+          flexibleSpace:  const Image(
             image: NetworkImage(
                 'https://static.wikia.nocookie.net/marvelcinematicuniverse/images/1/1d/Infinity_Stones_VFX.png/revision/latest?cb=20190427010810'),
 
@@ -31,9 +31,3 @@ void main() {
     ),
   );
 }
-//body: const Center( child:
-//Image(
-//image: NetworkImage(
-//'https://i.pinimg.com/originals/13/b1/78/13b1788b58e34a0eea149aab7219fb7f.jpg'),
-//),
-//),
